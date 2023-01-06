ba68b358577d39e72e69ef6630cc7663de312dee
bobbin054 <80533145+bobbin054@users.noreply.github.com>
2023-01-06 21:51:33 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })

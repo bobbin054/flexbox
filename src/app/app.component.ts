8674c899d93e30b2b8005eb982780577538bedfd
Joakim Östelid <joakim.ostelid@xylem.com>
2025-03-14 14:27:17 +0100

    staete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FlexDivsComponent } from "./components/flex-divs/flex-divs.component";
 import { FlexInputsComponent } from "./components/flex-inputs/flex-inputs.component";
+import { CheckboxComponent } from "./checkbox/checkbox.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FlexDivsComponent, FlexInputsComponent],
+  imports: [RouterOutlet, FlexDivsComponent, FlexInputsComponent, CheckboxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
